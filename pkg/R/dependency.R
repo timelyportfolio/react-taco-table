4c25af01424d1c096b74d5ebb274af356c29f300
timelyportfolio <kent.russell@timelyportfolio.com>
2016-06-26 09:11:15 -0500

    get dependencies working

--- pkg/R/dependency.R
+++ pkg/R/dependency.R
@@ -1,8 +1,12 @@
+#' Dependencies for react-taco-table
+#'
+#' @return \code{htmlDependency}
+#' @export
 html_dependency_tacotable <- function(){
   htmltools::htmlDependency(
     name="react-taco-table",
     version="0.1.1",
-    src=system.file("htmlwidgets/lib/react-taco-table/dist"),
+    src=system.file("htmlwidgets/lib/react-taco-table/dist", package="tacotableR"),
     script="react-taco-table.js",
     stylesheet="react-taco-table.css"
   )
---
+++ pkg/inst/examples/basic.R
@@ -0,0 +1,15 @@
+library(tacotableR)
+library(reactR)
+library(htmltools)
+
+browsable(
+  attachDependencies(
+    tagList(
+      
+    ),
+    list(
+      html_dependency_react(),
+      html_dependency_tacotable()
+    )
+  )
+)
